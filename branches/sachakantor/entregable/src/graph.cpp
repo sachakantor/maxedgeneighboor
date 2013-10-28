8528327f48efd9e7bb22ae14d861b5af539dda8b
Sacha Sebastian Kantor <sachakantor@users.noreply.github.com>
2013-10-28 16:46:28 +0000

    Updates IMPORTANTES a la estructura de la clase graph. Implementacion de los constructores y destructores (falta testear)

--- branches/sachakantor/entregable/src/graph.cpp
+++ branches/sachakantor/entregable/src/graph.cpp
@@ -11,17 +11,21 @@ using namespace std;
 node*   *******************************************************
 ***************************************************************/
 /*Constructores y Destructores*/
-node::node(uint id) :
-    _id(id),
-    _degree(0),
-    _neighbors()
-{}
-
-node::node(uint id,degree d) :
+node::node(const node_id id,const degree d,const uint max_degree) :
     _id(id),
     _degree(d),
-    _neighbors(d,NULL)
-{}
+    _neighbors()
+{
+    this->_neighbors.reserve(max_degree);
+}
+
+//node::node(uint id,degree d) :
+//    _id(id),
+//    _degree(d),
+//    _neighbors()
+//{
+//    this->_neighbors.reserve(d);
+//}
 
 node::node(const node& copy) :
     _id(copy._id),
@@ -33,11 +37,15 @@ node::~node(){}
 
 /*Operadores*/
 bool node::operator==(const node& v) const{
-    return this->_id == v._id;
+    return this->_id == v._id &&
+            this->_degree == v._degree &&
+            this->_neighbors == v._neighbors;
 }
 
 bool node::operator!=(const node& v) const{
-    return !(*this == v);
+    return this->_id != v._id ||
+            this->_degree != v._degree ||
+            this->_neighbors != v._neighbors;
 }
 
 bool node::operator<(const node& v) const{
@@ -48,57 +56,110 @@ bool node::operator<(const node& v) const{
 * Edge  *******************************************************
 ***************************************************************/
 /*Constructor*/
-edge::edge() : _nodeA(NULL), _nodeB(NULL){}
+edge::edge() : _node_id_A(0), _node_id_B(0){}
 
-edge::edge(const node& a, const node& b) :
-    _nodeA(&a),
-    _nodeB(&b)
+edge::edge(node_id a,node_id b) :
+    _node_id_A(a),
+    _node_id_B(b)
 {}
 
 edge::edge(const edge& e) :
-    _nodeA(e._nodeA),
-    _nodeB(e._nodeB)
+    _node_id_A(e._node_id_A),
+    _node_id_B(e._node_id_B)
 {}
 
 /*Operadores*/
 bool edge::operator==(const edge& e) const{
-    return this->_nodeA == e._nodeA && this->_nodeB == e._nodeB;
+    return this->_node_id_A == e._node_id_A && this->_node_id_B == e._node_id_B;
 }
 
 bool edge::operator!=(const edge& e) const{
-    return !(*this == e);
+    return this->_node_id_A != e._node_id_A || this->_node_id_B != e._node_id_B;
 }
 
 /***************************************************************
 * Graph *******************************************************
 ***************************************************************/
 /*Constructores y destructor*/
-graph::graph() : _quant_nodes(),_quant_edges(){}
+graph::graph() : _quant_nodes(0),_quant_edges(0){}
 
 graph::graph(const graph& copy) :
     _quant_nodes(copy._quant_nodes),
-    _quant_edges(copy._quant_edges)
+    _quant_edges(copy._quant_edges),
+    _adjacency_matrix(copy._adjacency_matrix),
+    _nodes(copy._quant_nodes,NULL),
+    _edges(copy._quant_edges,NULL)
 {
+    /* Copiamos los nodos*/
+    for(uint i = 0;i<_quant_nodes;++i){
+        this->_nodes[i] = new node(*copy._nodes[i]);
+    }
 
+    /* Copiamos los ejes */
+    for(uint i = 0;i<_quant_edges;++i){
+        this->_edges[i] = new edge(*copy._edges[i]);
+    }
 }
 
-graph::~graph(){}
+graph::graph(uint quant_nodes,uint quant_edges,vector<node_id>::const_iterator& it_edges_nodes) :
+    _quant_nodes(quant_nodes),
+    _quant_edges(quant_edges),
+    _adjacency_matrix(quant_nodes,vector<bool>(quant_nodes,false)),
+    _nodes(quant_nodes,NULL),
+    _edges(quant_edges,NULL)
+{
+    /*Variables locales*/
+    node_id a,b;
+
+    /*Nodos*/
+    for(node_id i = 0; i<quant_nodes; ++i){
+        this->_nodes[i] = new node(i+1,0,quant_nodes-1);
+    }
+
+    /*Ejes*/
+    for(uint edge = 0; edge<quant_edges; ++edge){
+        /*Leemos los extremos del eje*/
+        a = *it_edges_nodes;
+        ++it_edges_nodes;
+        b = *it_edges_nodes;
+        ++it_edges_nodes;
+
+        /*Creamos el eje y actualizamos la matriz de adyacencia*/
+        this->_edges[edge] = new struct edge(a,b);
+        this->_adjacency_matrix[a-1][b-1] = true;
+        this->_adjacency_matrix[b-1][a-1] = true;
+
+        /*Actualizamos la info de los nodos*/
+        ++this->_nodes[a-1]->_degree;
+        this->_nodes[a-1]->_neighbors.push_back(b);
+        ++this->_nodes[b-1]->_degree;
+        this->_nodes[b-1]->_neighbors.push_back(a);
+    }
+}
+
+graph::~graph(){
+    for(vector<node*>::iterator it; it<this->_nodes.end();++it)
+        delete *it;
+
+    for(vector<edge*>::iterator it; it<this->_edges.end();++it)
+        delete *it;
+}
 
 /*Getters*/
 
 /*Métodos publicos*/
-adjacent_nodes_ptr graph::cmf_backtracking() const{
-    return adjacent_nodes_ptr();
+adjacent_nodes_id graph::cmf_backtracking() const{
+    return adjacent_nodes_id();
 }
 
-adjacent_nodes_ptr graph::cmf_golosa() const{
-    return adjacent_nodes_ptr();
+adjacent_nodes_id graph::cmf_golosa() const{
+    return adjacent_nodes_id();
 }
 
-adjacent_nodes_ptr graph::cmf_busqueda_local() const{
-    return adjacent_nodes_ptr();
+adjacent_nodes_id graph::cmf_busqueda_local() const{
+    return adjacent_nodes_id();
 }
 
-adjacent_nodes_ptr graph::cmf_tabu_search() const{
-    return adjacent_nodes_ptr();
+adjacent_nodes_id graph::cmf_tabu_search() const{
+    return adjacent_nodes_id();
 }
